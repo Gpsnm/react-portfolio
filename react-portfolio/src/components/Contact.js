a002c63239ca600a9c7638abc1b9370474f65dd1
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-12 16:57:25 +0000

    created a contact me form

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -3,15 +3,57 @@ import '../css/Contact.css'
 
 function Contact() {
     return(
-         <nav>
-        <div className='footer'>
-          <ol>
-          <li><a href="./Home.js">Github</a></li>
-          <li><a href="./project.js">Linkedin</a></li>
-          <li><a href="./Contact.js">Email</a></li>
-          </ol>
-        </div>
-      </nav>
+      <div className="contact">
+
+        <section class="contact-wrap">
+  <form action="" class="contact-form">
+    <div class="col-sm-6">        
+    <h2>Contact Me</h2>
+      <div class="input-block">
+        <label for="">First Name</label>
+        <input type="text" class="form-control"></input>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="input-block">
+        <label for="">Last Name</label>
+        <input type="text" class="form-control"></input>
+      </div>
+    </div>
+    <div class="col-sm-12">
+      <div class="input-block">
+        <label for="">Email</label>
+        <input type="text" class="form-control"></input>
+      </div>
+    </div>
+    <div class="col-sm-12">
+      <div class="input-block">
+        <label for="">Message Subject</label>
+        <input type="text" class="form-control"></input>
+      </div>
+    </div>
+    <div class="col-sm-12">
+      <div class="input-block textarea">
+        <label for="">Drop your message here</label>
+        <textarea rows="3" type="text" class="form-control"></textarea>
+      </div>
+    </div>
+    <div class="col-sm-12">
+      <button class="square-button">Send</button>
+    </div>
+  </form>
+  </section>
+  <div className="footer">
+    <ul>
+      <li>Github</li>
+      <li>Github</li>
+      <li>Github</li>
+      <li>Github</li>
+
+    </ul>
+  </div>
+      </div>
+      
     )
 }
 
