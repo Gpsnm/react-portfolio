9235160f310fb2b5e8b63dd8d0be982ec23270ea
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-12 18:14:53 +0000

    finished card styles

--- react-portfolio/src/components/project.js
+++ react-portfolio/src/components/project.js
@@ -1,19 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 import'../css/Project.css'
-import  '../images/samueltuki.github.io_iRecipe_.png'
+import image from '../images/samueltuki.github.io_iRecipe_.png'
 
 function Project(props) {
   return (
-    <div className="card">
-      <div className="card-image"><image src={props.image}></image></div>
+    <div className="card-container">
+      <div className="card-image">
+        <img src={image} alt="project screen shot"></img>
+      </div>
       <div className="card-text">
         <h3>{props.title}</h3>
         <p>{props.description}</p>
       </div>
       <div className="card-links">
-        <a href={props.deployed}></a>
-        <a href={props.github}></a>
+        <a href={props.deployed}>Live</a>
+        <a href={props.github}>Github</a>
 
       </div>
     </div>
