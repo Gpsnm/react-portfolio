395f515ef29b439a61a99d29bba79a24c02cbfc2
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-12 16:28:43 +0000

    made home page full view height

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
 import "./index.css"
 import Home from './components/Home';
 import ProjectGallery from './components/ProjectGallery'
@@ -8,19 +7,23 @@ import Contact from './components/Contact';
 import NavRoute from './components/NavRoute';
 
 function App() {
-  return(
+  return (
     <Router>
-    <div>
-  <NavRoute />
-  <Routes>
-  <Header />
-  <Route path="/" element={<Home />}/>
-  <Route path="/" element={<ProjectGallery/>}/>
-  <Route path="/" element={ <Contact />}/>
-  </Routes>
-  </div>
-  </Router>
-  )
+      <div>
+        <NavRoute />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="ProjectGallery" element={<ProjectGallery />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+
 }
 
 export default App;
--- react-portfolio/src/components/NavRoute.js
+++ react-portfolio/src/components/NavRoute.js
@@ -1,59 +1,28 @@
 import React from "react";
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import '../css/NavRoute.css'
 
 const NavRoute = () =>{
       return (
-    <ul className="nav nav-tabs">
+    <ul className="nav">
       <li className="nav-item">
         <NavLink
           to="/"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
         >
           Home
         </NavLink>
       </li>
       <li className="nav-item">
         <NavLink
-          to="about"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          About
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="blog"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Blog
+          to="projectGallery"
+        >projects
         </NavLink>
       </li>
       <li className="nav-item">
         <NavLink
           to="contact"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
         >
-          Contact
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="contact/learn"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Learn
+         Contact
         </NavLink>
       </li>
     </ul>
