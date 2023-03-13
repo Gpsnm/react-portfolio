ba5af4c713a1fea62454d7f89a83d5767a3619f9
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-13 18:09:37 +0000

    finished contact me page

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -1,6 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import '../css/Contact.css'
+import {FaLinkedin} from 'react-icons/fa'
+import {BsGithub} from 'react-icons/bs'
+import {TfiEmail} from 'react-icons/tfi'
+import {FcDocument} from 'react-icons/fc'
+
 
 const ContactForm = () => {
   const [state, handleSubmit] = useForm("xgebzygw");
@@ -45,7 +51,11 @@ const ContactForm = () => {
     </form>
     <div className='socials'>
       <h2>How to Reach Me</h2>
-      <div className='Linkedin'>linkedin</div>
+      <div className='Linkedin'><a target="blank" href='https://www.linkedin.com/in/adam-willetts/'> <FaLinkedin /></a><h3>Linkedin</h3></div>
+      <div className='Linkedin'><a target="blank" href='https://github.com/Gpsnm'> <BsGithub /></a><h3>Github</h3></div>
+      <div className='Linkedin'><a  href='mailto:adam-willetts@hotmail.co.uk'><TfiEmail /></a><h3>Email</h3></div>
+      <div className='Linkedin'><a target="blank" href='https://docs.google.com/document/d/1d_NJ3nVNHfQVF68cXjsFXEPOsxXVzWZoVWMfsh3ReUc/edit?usp=sharing'><FcDocument /></a><h3>Resume</h3></div>
+
 
     </div>
     </div>
